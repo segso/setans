867bf9b2fc47688ce9f29c3fc886091364bf4083
SEG <segdev.mx@gmail.com>
2026-06-22 23:18:41 -0600

    feat(app): set minimum app window size to 300x300

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:window_manager/window_manager.dart';
 import 'app.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+  await windowManager.setMinimumSize(const Size(300, 300));
   runApp(
     const ProviderScope(
       child: SetansApp(),
